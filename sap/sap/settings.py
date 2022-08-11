71a9eef8eb9e40820028ab3ecff20c476696f00f
Vic-silo <62065794+Vic-silo@users.noreply.github.com>
2022-08-11 11:53:12 +0200

    Squashed commit of the following:
    commit 71e0f1421bdb9a12a7ed0850a81edc621062a197
    Author: Vic-silo <62065794+Vic-silo@users.noreply.github.com>
    Date:   Thu Aug 11 11:45:20 2022 +0200
    
        gitignore
    
    commit 20d5ec1e2b0877bb50fafdf0aab67ea4deeb112f
    Author: Vic-silo <62065794+Vic-silo@users.noreply.github.com>
    Date:   Thu Aug 11 11:34:28 2022 +0200
    
        secret key
    
    commit c1732d01897c233cf2c9e7a675ffb783402a3bae
    Author: Vic-silo <62065794+Vic-silo@users.noreply.github.com>
    Date:   Thu Aug 11 11:22:41 2022 +0200
    
        secret key

--- sap/sap/settings.py
+++ sap/sap/settings.py
@@ -15,8 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-PWD = os.environ['Django_SAP']
-
+PWD = os.getenv('Django_SAP')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
