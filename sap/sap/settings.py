365cd4e584841af822c3e221e1d7df9ecd9c346b
Vic-silo <62065794+Vic-silo@users.noreply.github.com>
2022-08-11 10:58:06 +0200

    initial commit

--- sap/sap/settings.py
+++ sap/sap/settings.py
@@ -11,16 +11,18 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+PWD = os.environ['Django_SAP']
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o)7@rerg)qfhs1w%%p^$@z114d@wfn+p1xgheexcn$qkdki2(%'
+SECRET_KEY = PWD
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
